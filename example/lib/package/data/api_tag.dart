3a12ce38ceb7b94c5174b375c79e2e1cbff8706e
yeweicheng <321555174@qq.com>
2021-12-23 20:46:17 +0800

    WTag 组件实现

--- example/lib/package/data/api_tag.dart
+++ example/lib/package/data/api_tag.dart
@@ -4,11 +4,137 @@ import 'package:wao_ui/src/data/w_tag.dart';
 class ApiTag extends StatelessWidget {
   const ApiTag({Key? key}) : super(key: key);
 
+  Row get basicGroup {
+    return Row(children: [
+      WTag(slots: WTagSlot('标签一')),
+      WTag(slots: WTagSlot('标签二'), props: WTagProp(type: 'success')),
+      WTag(slots: WTagSlot('标签三'), props: WTagProp(type: 'info')),
+      WTag(slots: WTagSlot('标签四'), props: WTagProp(type: 'warning')),
+      WTag(slots: WTagSlot('标签五'), props: WTagProp(type: 'danger'))
+    ]);
+  }
+
+  Row get removableGroup {
+    return Row(children: [
+      WTag(slots: WTagSlot('标签一'), props: WTagProp(closable: true)),
+      WTag(
+          slots: WTagSlot('标签二'),
+          props: WTagProp(closable: true, type: 'success')),
+      WTag(
+          slots: WTagSlot('标签三'),
+          props: WTagProp(closable: true, type: 'info')),
+      WTag(
+          slots: WTagSlot('标签四'),
+          props: WTagProp(closable: true, type: 'warning')),
+      WTag(
+          slots: WTagSlot('标签五'),
+          props: WTagProp(closable: true, type: 'danger'))
+    ]);
+  }
+
+  Row get editableGroup {
+    return Row(children: [
+      WTag(
+          on: WTagOn(click: () {}),
+          slots: WTagSlot('标签一'),
+          props: WTagProp(disableTransitions: false, closable: true)),
+      WTag(
+          on: WTagOn(click: () {
+            print('');
+          }, close: () {
+            print('关闭');
+          }),
+          slots: WTagSlot('标签二'),
+          props: WTagProp(
+              disableTransitions: false, closable: true, type: 'success')),
+      WTag(
+          on: WTagOn(click: () {}),
+          slots: WTagSlot('标签三'),
+          props: WTagProp(
+              disableTransitions: false, closable: true, type: 'info')),
+      WTag(
+          on: WTagOn(click: () {}),
+          slots: WTagSlot('标签四'),
+          props: WTagProp(
+              disableTransitions: false, closable: true, type: 'warning')),
+      WTag(
+          on: WTagOn(click: () {}),
+          slots: WTagSlot('标签五'),
+          props: WTagProp(
+              disableTransitions: false, closable: true, type: 'danger'))
+    ]);
+  }
+
+  Row get diffSizeGroup {
+    return Row(children: [
+      WTag(slots: WTagSlot('默认标签')),
+      WTag(slots: WTagSlot('中等标签'), props: WTagProp(size: 'medium')),
+      WTag(slots: WTagSlot('小型标签'), props: WTagProp(size: 'small')),
+      WTag(slots: WTagSlot('超小标签'), props: WTagProp(size: 'mini')),
+    ]);
+  }
+
+  Row get diffThemeGroupDark {
+    return Row(children: [
+      const Text('Dart'),
+      WTag(
+        slots: WTagSlot('标签一'),
+        props: WTagProp(effect: 'dark', closable: true),
+      ),
+      WTag(
+        slots: WTagSlot('标签二'),
+        props: WTagProp(effect: 'dark', type: 'success', closable: true),
+      ),
+      WTag(
+        slots: WTagSlot('标签三'),
+        props: WTagProp(effect: 'dark', type: 'info', closable: true),
+      ),
+      WTag(
+        slots: WTagSlot('标签四'),
+        props: WTagProp(effect: 'dark', type: 'warning', closable: true),
+      ),
+      WTag(
+        slots: WTagSlot('标签五'),
+        props: WTagProp(effect: 'dark', type: 'danger', closable: true),
+      )
+    ]);
+  }
+
+  Row get diffThemeGroupPlain {
+    return Row(children: [
+      const Text('Plain'),
+      WTag(
+        slots: WTagSlot('标签一'),
+        props: WTagProp(effect: 'plain'),
+      ),
+      WTag(
+        slots: WTagSlot('标签二'),
+        props: WTagProp(effect: 'plain', type: 'success'),
+      ),
+      WTag(
+        slots: WTagSlot('标签三'),
+        props: WTagProp(effect: 'plain', type: 'info'),
+      ),
+      WTag(
+        slots: WTagSlot('标签四'),
+        props: WTagProp(effect: 'plain', type: 'warning'),
+      ),
+      WTag(
+        slots: WTagSlot('标签五'),
+        props: WTagProp(effect: 'plain', type: 'danger'),
+      )
+    ]);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(children: [
-      const Text('WTag'),
-      WTag(),
+      basicGroup,
+      removableGroup,
+      editableGroup,
+      diffSizeGroup,
+      diffThemeGroupDark,
+      diffThemeGroupPlain,
     ]);
   }
 }
--- lib/src/data/w_tag.dart
+++ lib/src/data/w_tag.dart
@@ -3,6 +3,10 @@ import 'package:wao_ui/core/base_on.dart';
 import 'package:wao_ui/core/base_prop.dart';
 import 'package:wao_ui/core/base_slot.dart';
 import 'package:wao_ui/core/base_widget.dart';
+import 'package:wao_ui/core/utils/wrapper.dart';
+import 'package:wao_ui/src/basic/cfg_global.dart';
+import 'package:wao_ui/src/basic/w_button.dart';
+import 'package:bitsdojo_window/src/widgets/mouse_state_builder.dart';
 
 class WTag extends StatelessWidget
     implements BaseWidget<WTagOn, WTagProp, WTagSlot> {
@@ -28,13 +32,147 @@ class WTag extends StatelessWidget
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      decoration: decoration,
+      padding: padding,
+      child: Row(
+        children: [...tagMain, ...closeButton],
+      ),
+    );
+  }
+
+  EdgeInsets get padding {
+    return EdgeInsets.fromLTRB(paddingH, paddingV, paddingH, paddingV);
+  }
+
+  double get paddingV {
+    return cfgGlobal.padding.val($props.size!);
+  }
+
+  double get paddingH {
+    return paddingV / .4;
+  }
+
+  BoxDecoration get decoration {
+    BoxDecoration decoration = BoxDecoration(
+        color: backgroundColor,
+        border: WBorder.all($props.size, $props.type),
+        borderRadius: BorderRadius.all(
+            Radius.circular(cfgGlobal.borderRadius.val($props.size))));
+    return decoration;
+  }
+
+  Color get backgroundColor {
+    return $props.isDark
+        ? color
+        : $props.isPlain
+            ? Colors.white
+            : color.shade100;
+  }
+
+  Color get fontColor {
+    return $props.isDark ? Colors.white : color.shade700;
+  }
+
+  MaterialColor get color {
+    return cfgGlobal.color.val($props.type);
+  }
+
+  List<Widget> get closeButton {
+    return $props.closable
+        ? [
+            marginWrapper(
+              closeButtonMain,
+              EdgeInsets.only(left: paddingV),
+            )
+          ]
+        : [];
+  }
+
+  Color get closeButtonBackground {
+    return $props.isDark ? color.shade200 : color;
+  }
+
+  Widget get closeButtonMain {
+    return MouseStateBuilder(
+      builder: (context, state) {
+        return Listener(
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(30.0),
+            child: colorWrapper(
+              Icon(
+                Icons.close,
+                color: state.isMouseOver ? Colors.white : fontColor,
+                size: 16,
+              ),
+              closeButtonBackground,
+              state.isMouseOver,
+            ),
+          ),
+          onPointerUp: (e) => $on.close?.call(),
+        );
+      },
+    );
+  }
+
+  List<Widget> get tagMain {
+    if ($slots.defaultSlot != null) {
+      return $slots.defaultSlot!;
+    } else {
+      if ($slots.defaultSlotBefore is String) {
+        return [
+          Text(
+            $slots.defaultSlotBefore as String,
+            style: TextStyle(
+              color: fontColor,
+            ),
+          )
+        ];
+      }
+    }
+    return [];
   }
 }
 
-class WTagOn extends BaseOn {}
+class WTagOn extends BaseOn {
+  Function? click;
+  Function()? close;
+  WTagOn({this.click, this.close});
+}
 
-class WTagProp extends BaseProp {}
+class WTagProp extends BaseProp {
+  String? type;
+  late bool closable;
+  late bool disableTransitions;
+  late bool hit;
+  Color? color;
+  String? size;
+  String? effect;
+
+  WTagProp({
+    String? type,
+    bool? closable,
+    bool? disableTransitions,
+    bool? hit,
+    this.color,
+    String? size,
+    this.effect,
+  }) {
+    this.type = type ?? 'primary';
+    this.closable = closable ?? false;
+    this.disableTransitions = disableTransitions ?? false;
+    this.hit = hit ?? false;
+    this.size = size ?? 'small';
+  }
+
+  bool get isDark {
+    return effect == 'dark';
+  }
+
+  bool get isPlain {
+    return effect == 'plain';
+  }
+}
 
 class WTagSlot extends BaseSlot {
   WTagSlot(defaultSlotBefore) : super(defaultSlotBefore);
