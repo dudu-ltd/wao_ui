ff884c81e9b16683f6441ba36c57b5b7a68ec645
CorvusYe <CorvusY@foxmail.com>
2022-03-01 17:19:01 +0800

    改造菜单的 item 让其担任其 submenu title 的角色

--- lib/src/navigation/menu/w_menu_item.dart
+++ lib/src/navigation/menu/w_menu_item.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:wao_ui/core/base_on.dart';
 import 'package:wao_ui/core/base_prop.dart';
@@ -5,10 +8,19 @@ import 'package:wao_ui/core/base_slot.dart';
 import 'package:wao_ui/core/base_mixins.dart';
 import 'package:wao_ui/wao_ui.dart';
 
-class WMenuItem extends StatelessWidget
+class WMenuItem extends StatefulWidget
     with
         HasRootMenu,
         BaseMixins<WMenuItemOn, WMenuItemProp, WMenuItemSlot, WMenuItemStyle> {
+  late AnimationController expandController;
+  late AnimationController bgController;
+  late Animation<double> iconAngle;
+  late Animation<Color?> bgColor;
+  late ValueNotifier isHover = ValueNotifier(false);
+  late ValueNotifier isExpand = ValueNotifier(false);
+
+  WSubmenu? belongTo;
+
   WMenuItem({
     Key? key,
     WMenuItemOn? on,
@@ -24,36 +36,7 @@ class WMenuItem extends StatelessWidget
   }
 
   @override
-  Widget build(BuildContext context) {
-    print(rootMenu!.collapse.value);
-    return SizedBox(
-      height: lineHeight,
-      child: Padding(
-        padding: padding,
-        child: Align(
-          child: Row(
-            children: [
-              if ($hasDefalutSlot)
-                Padding(
-                  padding: const EdgeInsets.only(right: 15.0),
-                  child: $row,
-                ),
-              if ($slots.title != null)
-                Offstage(
-                  offstage: rootMenu!.collapse.value,
-                  child: stringToWidget.fn.call(
-                    $slots.title!,
-                    0,
-                    this,
-                  ),
-                )
-            ],
-          ),
-          alignment: Alignment.center,
-        ),
-      ),
-    );
-  }
+  State<WMenuItem> createState() => _WMenuItemState();
 
   SlotTranslator get stringToWidget {
     return SlotTranslator(
@@ -100,7 +83,200 @@ class WMenuItem extends StatelessWidget
   }
 }
 
-class WMenuItemOn extends BaseOn {}
+class _WMenuItemState extends State<WMenuItem> with TickerProviderStateMixin {
+  @override
+  void initState() {
+    // 设置展开动画
+    widget.expandController =
+        AnimationController(vsync: this, duration: Duration(milliseconds: 300));
+    widget.iconAngle = Tween(begin: 0.0, end: pi)
+        .animate(widget.expandController)
+      ..addListener(updateView);
+
+    // 设置鼠标浮入动画
+    widget.bgController =
+        AnimationController(vsync: this, duration: Duration(milliseconds: 300));
+    widget.bgColor = ColorTween(
+            begin: widget.rootMenu?.$style.backgroundColor,
+            end: widget.rootMenu?.$style.hoverBackgroundColor)
+        .animate(widget.bgController)
+      ..addListener(updateView);
+
+    valueChangeBind();
+    super.initState();
+  }
+
+  valueChangeBind() {
+    widget.rootMenu?.collapse.addListener(updateView);
+
+    // 触发展开动画
+    widget.isExpand.addListener(() {
+      if (widget.isExpand.value) {
+        Timer(
+          Duration(milliseconds: showTime),
+          () {
+            widget.expandController.forward();
+          },
+        );
+      } else {
+        Timer(
+          Duration(milliseconds: hideTime),
+          () {
+            widget.expandController.reverse();
+          },
+        );
+      }
+    });
+
+    // 触发鼠标浮入动画
+    widget.isHover.addListener(() {
+      if (widget.isHover.value) {
+        widget.bgController.forward();
+      } else {
+        widget.bgController.reverse();
+      }
+    });
+  }
+
+  int get showTime {
+    return timeByHover(widget.belongTo?.$props.showTimeout.toInt());
+  }
+
+  int get hideTime {
+    return timeByHover(widget.belongTo?.$props.hideTimeout.toInt());
+  }
+
+  int timeByHover(int? time) {
+    return expandByHover ? time ?? 0 : 0;
+  }
+
+  pointEnter(e) {
+    if (expandByHover) widget.isExpand.value = true;
+    widget.bgController.forward();
+    widget.$on.enter?.call(e);
+  }
+
+  pointExit(e) {
+    if (expandByHover) widget.isExpand.value = false;
+    widget.bgController.reverse();
+    widget.$on.exit?.call(e);
+  }
+
+  pointClick() {
+    if (!expandByHover) {
+      widget.isExpand.value = !widget.isExpand.value;
+      widget.$on.click?.call();
+    }
+  }
+
+  @override
+  void dispose() {
+    widget.expandController.dispose();
+    widget.bgController.dispose();
+    widget.isExpand.dispose();
+    widget.isHover.dispose();
+    super.dispose();
+  }
+
+  updateView() => setState(() {});
+
+  @override
+  Widget build(BuildContext context) {
+    var itemBody = Row(
+      children: [...defaultAsPrefix, ...title, ...suffix],
+    );
+    var box = boxWrapper(itemBody);
+    return eventWrapper(box);
+  }
+
+  Widget eventWrapper(Widget child) {
+    return MouseRegion(
+      onEnter: pointEnter,
+      onExit: pointExit,
+      child: InkWell(
+        child: child,
+        onTap: pointClick,
+      ),
+    );
+  }
+
+  Widget boxWrapper(Widget child) {
+    return ColoredBox(
+      color: widget.bgColor.value ??
+          widget.rootMenu?.$style.backgroundColor ??
+          Colors.transparent,
+      child: SizedBox(
+        height: widget.lineHeight,
+        child: Padding(
+          padding: widget.padding,
+          child: child,
+        ),
+      ),
+    );
+  }
+
+  List<Widget> get defaultAsPrefix {
+    return <Widget>[
+      if (widget.$hasDefalutSlot && iconNeedPadding)
+        Padding(
+          padding: const EdgeInsets.only(right: 5.0),
+          child: widget.$row,
+        ),
+      if (widget.$hasDefalutSlot && !iconNeedPadding) widget.$row,
+    ];
+  }
+
+  List<Widget> get title {
+    return <Widget>[
+      if (widget.$slots.title != null)
+        Expanded(
+          child: Offstage(
+            offstage: widget.rootMenu!.collapse.value,
+            child:
+                widget.stringToWidget.fn.call(widget.$slots.title!, 0, widget),
+          ),
+        ),
+    ];
+  }
+
+  List<Widget> get suffix {
+    return <Widget>[
+      if (needSuffix)
+        Padding(
+          padding: const EdgeInsets.only(left: 8.0),
+          child: Transform.rotate(
+            angle: widget.iconAngle.value,
+            child: Icon(
+              widget.$slots.suffix,
+              size: 16,
+              color: widget.rootMenu?.$style.color,
+            ),
+          ),
+        ),
+    ];
+  }
+
+  bool get expandByHover {
+    return widget.rootMenu != null &&
+        widget.rootMenu!.$props.modeIsHorizontal &&
+        widget.rootMenu!.$props.triggerIsHover;
+  }
+
+  bool get needSuffix {
+    return widget.$slots.suffix != null && !widget.rootMenu!.collapse.value;
+  }
+
+  bool get iconNeedPadding {
+    return widget.$first is Icon && !widget.rootMenu!.$props.collapse;
+  }
+}
+
+class WMenuItemOn extends BaseOn {
+  Function(dynamic)? enter;
+  Function(dynamic)? exit;
+  Function()? click;
+  WMenuItemOn({this.enter, this.exit, this.click});
+}
 
 class WMenuItemProp extends BaseProp {
   String? index;
@@ -115,8 +291,10 @@ class WMenuItemProp extends BaseProp {
 
 class WMenuItemSlot extends BaseSlot {
   String? title;
+  IconData? suffix;
   WMenuItemSlot(
     defaultSlotBefore, {
     this.title,
+    this.suffix,
   }) : super(defaultSlotBefore);
 }
--- lib/src/navigation/menu/w_submenu.dart
+++ lib/src/navigation/menu/w_submenu.dart
@@ -13,14 +13,7 @@ class WSubmenu extends StatefulWidget
         HasRootMenu,
         BaseMixins<WSubmenuOn, WSubmenuProp, WSubmenuSlot, WSubmenuStyle> {
   late AnimationController expandController;
-  late AnimationController bgController;
-  late Animation<double> iconAngle;
   late Animation<double> itemsPanelHeight;
-
-  late Animation<Color?> bgColor;
-
-  late ValueNotifier isExpand = ValueNotifier(false);
-  late ValueNotifier isHover = ValueNotifier(false);
 
   WSubmenu({
     Key? key,
@@ -68,118 +61,23 @@ class _WSubmenuState extends State<WSubmenu> with TickerProviderStateMixin {
   void initState() {
     widget.expandController =
         AnimationController(vsync: this, duration: Duration(milliseconds: 300));
-    widget.bgController =
-        AnimationController(vsync: this, duration: Duration(milliseconds: 300));
-    widget.iconAngle = Tween(begin: 0.0, end: pi)
-        .animate(widget.expandController)
-      ..addListener(updateView);
     widget.itemsPanelHeight = Tween(begin: 0.0, end: 200.0)
         .animate(widget.expandController)
       ..addListener(updateView);
-    widget.bgColor = ColorTween(
-            begin: widget.rootMenu?.$style.backgroundColor,
-            end: widget.rootMenu?.$style.hoverBackgroundColor)
-        .animate(widget.bgController)
-      ..addListener(updateView);
-    valueChangeBind();
     super.initState();
   }
 
   updateView() => setState(() {});
 
-  valueChangeBind() {
-    widget.rootMenu?.collapse.addListener(updateView);
-    widget.isHover.addListener(() {
-      if (widget.isHover.value) {
-        widget.bgController.forward();
-      } else {
-        widget.bgController.reverse();
-      }
-    });
-    widget.isExpand.addListener(() {
-      if (widget.isExpand.value) {
-        Timer(Duration(milliseconds: widget.$props.showTimeout.toInt()), () {
-          widget.expandController.forward();
-        });
-      } else {
-        Timer(Duration(milliseconds: widget.$props.hideTimeout.toInt()), () {
-          widget.expandController.reverse();
-        });
-      }
-    });
-  }
-
   @override
   void dispose() {
     widget.expandController.dispose();
-    widget.bgController.dispose();
-    widget.isExpand.dispose();
-    widget.isHover.dispose();
     super.dispose();
-  }
-
-  pointEnter(e) {
-    if (expandOnHover) widget.isExpand.value = true;
-    widget.isHover.value = true;
-  }
-
-  pointExit(e) {
-    if (expandOnHover) widget.isExpand.value = false;
-    widget.isHover.value = false;
-  }
-
-  pointClick() {
-    widget.isExpand.value = !widget.isExpand.value;
-  }
-
-  bool get expandOnHover {
-    return widget.rootMenu != null &&
-        widget.rootMenu!.$props.modeIsHorizontal &&
-        widget.rootMenu!.$props.triggerIsHover;
   }
 
   @override
   Widget build(BuildContext context) {
-    var box = ColoredBox(
-      color: widget.bgColor.value ??
-          widget.rootMenu?.$style.backgroundColor ??
-          Colors.white.withAlpha(0),
-      child: Padding(
-        padding: const EdgeInsets.fromLTRB(20.0, 0.0, 20.0, 0.0),
-        child: SizedBox(
-          height: widget.lineHeight,
-          child: Align(
-            child: Row(
-              children: [
-                titleContent,
-                if (!widget.rootMenu!.collapse.value)
-                  Padding(
-                    padding: const EdgeInsets.only(left: 8.0),
-                    child: Transform.rotate(
-                      angle: widget.iconAngle.value,
-                      child: Icon(
-                        Icons.expand_more,
-                        size: 16,
-                        color: widget.rootMenu?.$style.color,
-                      ),
-                    ),
-                  ),
-              ],
-            ),
-            alignment: Alignment.center,
-          ),
-        ),
-      ),
-    );
-
-    return MouseRegion(
-      onEnter: pointEnter,
-      onExit: pointExit,
-      child: InkWell(
-        child: box,
-        onTap: pointClick,
-      ),
-    );
+    return title;
   }
 
   double get panelBorder {
@@ -201,7 +99,12 @@ class _WSubmenuState extends State<WSubmenu> with TickerProviderStateMixin {
   Widget get title {
     widget.$slots.title?.rootMenu = widget.rootMenu;
     if (widget.$slots.title != null) {
-      widget.$slots.title!.$style.padding = EdgeInsets.zero;
+      if (widget.$slots.title is WMenuItem) {
+        widget.$slots.title!.$slots.suffix =
+            widget.$slots.title!.$slots.suffix ?? Icons.expand_more;
+        widget.$slots.title!.rootMenu = widget.rootMenu;
+        widget.$slots.title!.belongTo = widget;
+      }
       return widget.$slots.title!;
     } else {
       return const Text('');
