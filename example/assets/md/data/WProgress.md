8060c5d0fc88e50f44cc1f608cd1283a09c95333
CorvusYe <CorvusY@foxmail.com>
2022-04-05 02:53:23 +0800

    进度条 demo

--- example/assets/md/data/WProgress.md
+++ example/assets/md/data/WProgress.md
@@ -1,1 +1,47 @@
 # 进度条
+## 直线
+### 基本用法
+```widget
+progressLine
+```
+
+### 格式化数值
+```widget
+progressLineFormat
+```
+
+### 状态位进度条
+```widgetsRow
+progressLineStatus1,progressLineStatus2,progressLineStatus3
+```
+
+### 数值内置进度条
+```widget
+progressLineInner
+```
+
+### 携带状态位的数值内置进度条
+```widgetsRow
+progressLineInnerStatus1,progressLineInnerStatus2,progressLineInnerStatus3
+```
+
+### 设置颜色
+```widgetsRow
+progressColor1,progressColor2,progressColor3
+```
+
+## 环型
+### 基本环形
+```widgets
+progressCircle1,progressCircle2
+```
+
+### 环形状态
+```widgetsRow
+progressCircleStatus1,progressCircleStatus2,progressCircleStatus3
+```
+
+## 仪表盘
+```widget
+progressDashboard
+```
--- example/lib/views/index.dart
+++ example/lib/views/index.dart
@@ -149,7 +149,7 @@ class _IndexPageState extends State<IndexPage> {
   Navigator createNav() {
     return Navigator(
       // Navigator
-      initialRoute: 'data/WProgress',
+      initialRoute: 'data/WResult',
       onGenerateRoute: (val) {
         RoutePageBuilder builder = getNext(val.name!);
         return PageRouteBuilder(
@@ -395,7 +395,7 @@ regist() {}''');
           // {"id": "WDescriptions", "text": "对象描述"},
           // {"id": "WEmpty", "text": "空提醒"},
           // {"id": "WPagination", "text": "分页"},
-          {"id": "WProgress", "text": "进度条"},
+          // {"id": "WProgress", "text": "进度条"},
           {"id": "WResult", "text": "结果"},
           {"id": "WSkeleton", "text": "页面骨架"},
           {"id": "WTable", "text": "表格"},
